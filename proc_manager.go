619c46c82f437771f4641ec8e003a3d23ddc335d
Clifford Richardson <cmrallen@gmail.com>
2016-04-08 23:53:31 -0700

    first commit, added some stuff related to starting processes

---
+++ proc_manager.go
@@ -0,0 +1,70 @@
+package ggi
+
+import (
+	"encoding/gob"
+	"log"
+	"net"
+	"net/http"
+	"os"
+)
+
+var processManagers = []*PManager{}
+
+//PManager is the primary process manager
+type PManager struct {
+	processes []*Process
+	socket    *net.UnixListener
+}
+
+//intermediary function
+func (p *PManager) send(r *http.Request) *http.Response {
+	return nil
+}
+
+//spawn a process and setup the connection
+func (p *PManager) spawnProcess(path string) {
+	process, err := os.StartProcess(path, nil, &os.ProcAttr{})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	ggiProc := &Process{}
+	ggiProc.process = process
+	ggiProc.connection, err = p.socket.AcceptUnix()
+	if err != nil {
+		log.Println(err)
+	}
+
+	ggiProc.encoder = gob.NewEncoder(ggiProc.connection)
+	ggiProc.decoder = gob.NewDecoder(ggiProc.connection)
+	p.processes = append(p.processes, ggiProc)
+}
+
+func spawnNewManager() *PManager {
+	IPCAddress, err := net.ResolveUnixAddr("unixpacket", "/var/run/ggi.sock")
+	if err != nil {
+		log.Println(err)
+	}
+	IPCListener, err := net.ListenUnix("unixpacket", IPCAddress)
+	if err != nil {
+		//likely reached due to an already-existing socket. attempt to remove and
+		//create
+		os.Remove("/var/run/ggi.sock")
+		IPCListener, err = net.ListenUnix("unixpacket", IPCAddress)
+		if err != nil {
+			//halt the process and return nothing
+			log.Println(err)
+			return nil
+		}
+		log.Println(err)
+	}
+
+	pm := &PManager{
+		[]*Process{},
+		IPCListener,
+	}
+	processManagers = append(processManagers, pm)
+
+	return pm
+}
---
+++ process.go
@@ -0,0 +1,20 @@
+package ggi
+
+import (
+	"encoding/gob"
+	"net"
+	"os"
+)
+
+//Process describes a process to be used for request handling
+type Process struct {
+	ID         int
+	process    *os.Process
+	connection *net.UnixConn
+	encoder    *gob.Encoder
+	decoder    *gob.Decoder
+}
+
+func (p *Process) kill() {
+	p.process.Kill()
+}
